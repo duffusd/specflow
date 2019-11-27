3328f8cc48947dc3b4ec69ae7af19a0bf09e2c29
Cameron Lyon <CLyon@R1RCM.COM>
2019-11-27 12:04:27 -0700

    setup

---
+++ SpecFlowTest/Bindings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTest
+{
+	[Binding]
+	public class Bindings
+	{
+		private int _accountNumber;
+		[Given(@"Patient Account (.*) Exists in (\S+) database")]
+		public void GivenPatientAccountExistsInTempDatabase(int p0, string p2)
+		{
+			_accountNumber = p0;
+		}
+
+		[When(@"get patient account from PatientAccountService")]
+		public void WhenGetPatientAccountFromPatientAccountService()
+		{
+		}
+
+		[Then(@"Patient Account Information Should Be Retrieved")]
+		public void ThenPatientAccountInformationShouldBeRetrieved()
+		{
+			_accountNumber.Should().BeGreaterOrEqualTo(10);
+		}
+
+		[Then(@"Validated")]
+		public void ThenValidated()
+		{
+		}
+
+	}
+}
---
+++ SpecFlowTest/Example.feature
@@ -0,0 +1,10 @@
+﻿Feature: PatientAccountCases
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Get Patient Account From R1.Insight.PatientAccount Service
+Given Patient Account 636573 Exists in temp database
+When get patient account from PatientAccountService
+Then Patient Account Information Should Be Retrieved
+And Validated
